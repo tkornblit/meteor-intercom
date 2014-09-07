8a20d0e8fff177826856c6d738eeae163cf3d69c
Tom Kornblit <tom.kornblit@yahoo.com>
2014-09-07 11:07:28 -0400

    Wrap client side code with settings conditional.

--- intercom_client.js
+++ intercom_client.js
@@ -1,52 +1,55 @@
-Meteor.subscribe('intercomHash');
+IntercomSettings = {};
 
-var minimumUserInfo = function(user) {
-  var info = {
-    app_id: Meteor.settings.public.intercom.id,
-    
-    user_id: user._id,
-    
-    created_at: Math.round(Meteor.user().createdAt/1000)
-  }
-  
-  // they actually need to have this but it can be useful for testing
-  if (user.intercomHash)
-    info.user_hash = user.intercomHash;
-  
-  return info;
+if (Meteor.settings && Meteor.settings.public && Meteor.settings.public.intercom && Meteor.settings.public.intercom.id) {
+
+    Meteor.subscribe('intercomHash');
+
+    var minimumUserInfo = function(user) {
+        var info = {
+            app_id: Meteor.settings.public.intercom.id,
+
+            user_id: user._id,
+
+            created_at: Math.round(Meteor.user().createdAt / 1000)
+        };
+
+        // they actually need to have this but it can be useful for testing
+        if (user.intercomHash)
+            info.user_hash = user.intercomHash;
+
+        return info;
+    };
+
+
+
+    IntercomSettings.minimumUserInfo = minimumUserInfo;
+
+    var booted = false;
+
+    // send data to intercom
+    Meteor.startup(function() {
+        Deps.autorun(function() {
+            var user = Meteor.user();
+            if (!user) // "log out"
+                return Intercom('shutdown');
+
+            var info = IntercomSettings.minimumUserInfo(user);
+            if (IntercomSettings.userInfo) {
+                var ready = IntercomSettings.userInfo(user, info);
+                if (ready === false)
+                    return;
+            }
+
+
+            if (info) {
+                var type = booted ? 'update' : 'boot';
+
+                // console.log(type, info)
+                Intercom(type, info);
+
+                booted = true;
+            }
+        });
+    });
+
 }
-
-
-
-IntercomSettings = {
-  // if you want to manually call it
-  minimumUserInfo: minimumUserInfo
-}
-
-var booted = false;
-
-// send data to intercom
-Meteor.startup(function() {
-  Deps.autorun(function() {
-    var user = Meteor.user();
-    if (!user) // "log out"
-      return Intercom('shutdown');
-  
-    var info = IntercomSettings.minimumUserInfo(user);
-    if (IntercomSettings.userInfo) {
-      var ready = IntercomSettings.userInfo(user, info);
-      if (ready === false)
-        return;
-    }
-      
-    
-    if (info) {
-      var type = booted ? 'update': 'boot';
-      
-      // console.log(type, info)
-      Intercom(type, info);
-      
-      booted = true;
-    }
-  });
-})
